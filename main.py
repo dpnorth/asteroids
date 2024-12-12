65da21fe940d259f39d15d13d71fc8d52810d04d
Don_Phillips <don.phillips@broadcom.com>
2024-12-12 15:43:25 -0700

    Ch2 Ex2 Display initialization

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    init_tup = pygame.init()
+    print(f"{init_tup[0]} passed and {init_tup[1]} failed")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        screen.fill(000000)
+        pygame.display.flip()
     
 if __name__ == "__main__":
     main()
