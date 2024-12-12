40ebea771326f7a2a7b7b1d30e557788d145adbd
Don_Phillips <don.phillips@broadcom.com>
2024-12-12 15:53:39 -0700

    Ch2 Ex3 tone down the refresh and the system resource usage

--- main.py
+++ main.py
@@ -9,12 +9,17 @@ def main():
     init_tup = pygame.init()
     print(f"{init_tup[0]} passed and {init_tup[1]} failed")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0
+
+    # Gameloop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(000000)
         pygame.display.flip()
+        dt = game_clock.tick(60) / 1000
     
 if __name__ == "__main__":
     main()
