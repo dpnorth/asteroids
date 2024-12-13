a797db57547fb82f0685f32f90bbc02c0b669389
Don_Phillips <don.phillips@broadcom.com>
2024-12-12 17:40:08 -0700

    Ch3 Ex1 added asteroid and asteroid field classes and created a container for asteroids and initialized the asteroid field

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import pygame
 from constants import *
 from player import *
+from asteroidfield import *
 
 def main():
     print("Starting asteroids!")
@@ -14,8 +15,12 @@ def main():
     dt = 0
     updateable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updateable, drawable)
+    Asteroid.containers = (asteroids, updateable, drawable)
+    AsteroidField.containers = (updateable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
 
     # Gameloop
     while True:
